fcb3f9e337bd4ee9c5bcff5c0c25859d7024a7cb
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2023-05-06 18:35:12 +0200

    Added global scope to mapped drives, allow mounting drive from script

--- NAS.ps1
+++ NAS.ps1
@@ -1,10 +1,10 @@
-$cred= Get-Credential -Credential Contoso\ServiceAccount
+#$cred= Get-Credential -Credential Contoso\ServiceAccount
 
 # Map NAS shares to drive
-New-PSDrive -Name "U" -Root "\\192.168.3.4\Backup" -Persist -PSProvider "FileSystem" -Credential $cred
+New-PSDrive -Name "U" -Root "\\192.168.3.4\Backup" -Persist -PSProvider "FileSystem" -Scope Global
 
 # Add shortcut to desktop
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Backup (CASABOLLA).lnk")
-$Shortcut.TargetPath = "U:"
+$Shortcut.TargetPath = "U:\"
 $Shortcut.Save()
