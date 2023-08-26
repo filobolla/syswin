6aba23a5208a978abd429c5d1126521ffddc9e50
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2023-08-26 18:36:02 +0200

    Added taskbar and start customization via LayoutModification file

--- taskbar.ps1
+++ taskbar.ps1
@@ -1,12 +1,18 @@
-# Taskbar.ps1
-# Add programs to taskbar via PinUtil: https://github.com/ChrisRfr/PinUtil
+# taskbar.ps1
+# Add programs to taskbar by applying a custom start layout (also sets start layout options)
+# After that both start menu and taskbar can be further modified (registry reset would restore previously generated custom layout)
 
-$pin_to_taskbar=@(
-    "%windir\System32\WindowsPowerShell\v1.0\powershell.exe"
-    "Microsoft.Windows.Photos_8wekyb3d8bbwe!App"
-)
+# Copy custom layout to the user config folder
+$shell_path="$env:userprofile\AppData\Local\Microsoft\Windows\Shell"
+Copy-Item "LayoutModification.xml" -Destination $shell_path
 
-foreach($pin in $pin_to_taskbar){
-    Write-Host "pinning"$pin"..."
-    Invoke-Expression -Command 'C:\Users\Filippo\Downloads\PinUtil.exe TaskBar' $pin
-}
+# Reset Start menu present settings
+Remove-Item -Force -Recurse -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount"
+# Reset Taskbar present settings
+Remove-Item -Force -Recurse -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband"
+
+# Restart Explorer to apply changes
+Get-Process explorer | Stop-Process
+
+Write-Host "Start and taskbar layout have been applied. 
+If modification on the device are made, make sure to update the layout model accordingly!" -ForegroundColor Yellow
