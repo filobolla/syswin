86ae8bf92c242235167d57ec0be5d2bffabdc29d
Filippo Bollarino <13319932+filobolla@users.noreply.github.com>
2023-08-26 18:37:54 +0200

    Housekeeping (colored output, file ordering)

--- VPN.ps1
+++ VPN.ps1
@@ -1,11 +1,11 @@
 ### Setup OpenVPN for Windows and import files
 
 If (!(Test-Path "C:\Program Files\OpenVPN\bin\openvpn-gui.exe")){
-    Write-Host "OpenVPN GUI not installed, skipping..."
+    Write-Host "OpenVPN GUI not installed, skipping..." -ForegroundColor Red
     return $false;
 }
 
-$Path="$env:userprofile\OpenVPN\config\"
+$config_path="$env:userprofile\OpenVPN\config\"
 
 If (!(Test-Path $Path)) {
     Write-Host "OpenVPN path was not found, Creating..."
@@ -14,5 +14,5 @@ If (!(Test-Path $Path)) {
 
 # Copy configuration from NAS to OpenVPN folder
 $NAS_src= Get-PSDrive | findstr "Public" | ForEach-Object {($_ -split ' ')[0]}
-robocopy "${NAS_src}:\Filippo\dotfiles_backup\vpn_config" "$path" filippo.ovpn
-robocopy "${NAS_src}:\Filippo\dotfiles_backup\vpn_config\openvpn_ufficio" "$path" VPNConfig.ovpn
+robocopy "${NAS_src}:\Filippo\dotfiles_backup\vpn_config" "$config_path" filippo.ovpn
+robocopy "${NAS_src}:\Filippo\dotfiles_backup\vpn_config\openvpn_ufficio" "$config_path" VPNConfig.ovpn
--- customize.ps1
+++ customize.ps1
@@ -5,15 +5,15 @@
 # 1. import Custom layout to C:\Users\%username%\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml
 ## NOT WORKING WITHOUT GROUP POLICY EDIT OR NEW USER CREATION
 
-#######################################
-##   Explorer, Start menu, Taskbar   ##
-#######################################
+#################
+##   Taskbar   ##
+#################
 
 # Enable dark theme
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name AppsUseLightTheme -Value 0
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name SystemUsesLightTheme -Value 0
-# Disable Recent app in start folder
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name Start_TrackProgs -Value 0
+# Disable Recently added apps in start folder TODO
+Set-ItemProperty -Path "" -Name  -Value 1
 # Hide Cortana Button
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowCortanaButton -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name OnboardUnpinCortana -Value 1
@@ -21,6 +21,11 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name PeopleBand -Value 0
 # Hide Task View button in taskbar
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowTaskViewButton -Value 0
+
+##################
+##   Explorer   ##
+##################
+
 # Hide frequent and recent files from Quick Access
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowRecent -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowFrequent -Value 0
@@ -31,11 +36,11 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 # My Computer icon
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0
 # Control Panel icon
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" -Value 1
 # User Files icon
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" -Value 0
 # Network icon
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" -Value 1
 # Recycle Bin icon
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{645FF040-5081-101B-9F08-00AA002F954E}" -Value 0
 
@@ -44,3 +49,5 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 
 
 # Pin Programs to taskbar by adding link to %appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar
+
+Get-Process explorer | Stop-Process
